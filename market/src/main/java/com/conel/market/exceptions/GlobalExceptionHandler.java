c37294c49a1f6bb4cbd3d8fd1842d8c218293214
LI123456mo <conellimo@gmail.com>
2026-02-13 15:06:36 +0300

    feat: add password match validation and global exception handling

--- market/src/main/java/com/conel/market/exceptions/GlobalExceptionHandler.java
+++ market/src/main/java/com/conel/market/exceptions/GlobalExceptionHandler.java
@@ -2,16 +2,45 @@ package com.conel.market.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.View;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private final View error;
+
+    public GlobalExceptionHandler(View error) {
+        this.error = error;
+    }
+
     @ExceptionHandler(RuntimeException.class)
+
+    // Catch manual logic errors (like "Email already exists")
     public ResponseEntity<String> handleRuntime(RuntimeException ex){
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
-    
+    //CATCHES VALIDATION ERRORS
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleValidationExceptions(MethodArgumentNotValidException ex){
+        Map<String,String> errors=new HashMap<>();
+
+        //FIELD ERRORS LIKE (@Email,@NotBlank)
+        ex.getBindingResult().getFieldErrors().forEach(error->
+                errors.put(error.getField(),error.getDefaultMessage())
+        );
+
+
+        //GLOBAL ERRORS(eg @PasswordMatch)
+        ex.getBindingResult().getGlobalErrors().forEach(error->
+                errors.put("global",error.getDefaultMessage())
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
 }
