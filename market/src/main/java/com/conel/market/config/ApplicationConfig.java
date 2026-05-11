e3b275b6441e1ea7cf165b38b0454241faae8823
unknown <conellimo@gmail.com>
2026-05-11 09:00:57 +0300

    deleted two docker files

--- market/src/main/java/com/conel/market/config/ApplicationConfig.java
+++ market/src/main/java/com/conel/market/config/ApplicationConfig.java
@@ -25,14 +25,14 @@ public class ApplicationConfig {
     }
 
     @Bean
-    public AuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider authProvider=new DaoAuthenticationProvider(userDetailsService());
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService());
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)throws Exception{
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config){
         return config.getAuthenticationManager();
     }
 
