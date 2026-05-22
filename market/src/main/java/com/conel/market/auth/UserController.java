e946ea6e5f905d2a125635356208b90dca38c377
unknown <conellimo@gmail.com>
2026-05-22 08:43:25 +0300

    configure springdoc openapi dependency for swagger documentation

--- market/src/main/java/com/conel/market/auth/UserController.java
+++ market/src/main/java/com/conel/market/auth/UserController.java
@@ -12,21 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/v1/users")
+@Tag()
 public class UserController {
-    private final AuthenticationService service;
-
-    @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(
-            @Valid @RequestBody RegisterRequest request
-    ){
-        return ResponseEntity.ok(service.register(request));
-    }
-
-    @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> authenticate(
-            @RequestBody AuthenticationRequest request
-    ){
-        return ResponseEntity.ok(service.authenticate(request));
-    }
 }
