a8e1f699d7aa6b39c328f653845ba8a55d475d64
unknown <conellimo@gmail.com>
2026-07-01 14:55:48 +0300

    added the N+1 optimization in product

--- market/src/main/java/com/conel/market/models/products/ProductRepository.java
+++ market/src/main/java/com/conel/market/models/products/ProductRepository.java
@@ -1,28 +1,24 @@
+
 package com.conel.market.models.products;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface ProductRepository extends JpaRepository<Product, String>, JpaSpecificationExecutor<Product> {
+public interface ProductRepository extends JpaRepository<Product, String> {
 
+    @EntityGraph(attributePaths = {"category", "seller"})
+    Optional<Product> findById(String id);
+
+    @EntityGraph(attributePaths = {"category", "seller"})
     List<Product> findAllByNameContainingIgnoreCase(String name);
 
-    List<Product> findAllByNameIgnoreCaseStartingWith(String name);
-
-    List<Product> findAllByNameIgnoreCaseEndsWith(String name);
-
-    List<Product> findAllByNameInIgnoreCase(List<String> name);
-
     @Modifying
-    @Query("""
-            UPDATE Product p 
-            SET p.active = false 
-            WHERE p.category.id = :categoryId
-           """)
+    @Query("UPDATE Product p SET p.active = false WHERE p.category.id = :categoryId")
     void archiveAllByCategoryId(@Param("categoryId") String categoryId);
 }
